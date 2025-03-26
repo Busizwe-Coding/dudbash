6a3cd018fe0d48d1356711868f666d47bf737f98
Busizwe-Coder <busizwe.dl@gmail.com>
2025-03-26 20:27:51 +0200

    New colour inheritance example

---
+++ colourInheritanceExample/colourInheritanceExample/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class colour
+{
+    public virtual void showColour(string colourPicked)
+    {
+        Console.WriteLine("The base colour will be: "+colourPicked);
+    }
+}
+class otherColour : colour
+{
+    public override void showColour(string colourPicked)
+    {
+        Console.WriteLine("Ther undertone will be: "+colourPicked);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("For this painting...");
+
+        colour mainColour = new colour();
+        mainColour.showColour("Red");
+
+        otherColour newColour = new otherColour();  
+        newColour.showColour("Brown");
+    }
+}
