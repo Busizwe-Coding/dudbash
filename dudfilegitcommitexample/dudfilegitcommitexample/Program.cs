ddd29d76723c13f850075d9475dbb1d962388326
Busizwe-Coder <busizwe.dl@gmail.com>
2025-03-26 18:50:38 +0200

    Added main method

--- dudfilegitcommitexample/dudfilegitcommitexample/Program.cs
+++ dudfilegitcommitexample/dudfilegitcommitexample/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello Github!");
+using System;
+class Program
+{
+    //moved all greetings into void method
+    static void allGreetings ()
+    {
+        string greetings = "Hello world";
+        string secondgreetings = "Hello Github!";
+        Console.WriteLine(greetings);
+        Console.WriteLine(secondgreetings);
+    }
+    
+    static void Main(string[] args)
+    {
+        //call greetings
+        allGreetings();
+    }
+}
+
